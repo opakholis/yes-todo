f5b97d36298b3e01fff0980f0a65d9e87f6a0398
Opa Kholis Majid <opakholis.id@gmail.com>
2021-11-03 02:22:46 +0700

    add margin between task

--- components/Task.tsx
+++ components/Task.tsx
@@ -9,13 +9,13 @@ type TaskProps = {
 export default function Task(props: TaskProps) {
   return (
     <View
-      style={tw`bg-white flex-row justify-between items-center rounded-lg p-4`}
+      style={tw`bg-white flex-row justify-between items-center rounded-lg p-5 mb-4`}
     >
       <View style={tw`flex-row items-center`}>
-        <View style={tw`bg-blue-300 w-6 h-6 rounded-md mr-3`} />
+        <View style={tw`bg-blue-200 w-7 h-7 rounded-md mr-3`} />
         <Text>{props.text}</Text>
       </View>
-      <View style={tw`border border-blue-600 w-4 h-4 rounded-full`} />
+      <View style={tw`border border-blue-700 w-4 h-4 rounded-full`} />
     </View>
   );
 }
