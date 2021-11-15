336e4164b1a7ff8c2c7c9bdc7cbd33e90f3468cb
Opa Kholis Majid <opakholis.id@gmail.com>
2021-11-15 07:47:00 +0700

    setup prettier and tsconfig (#1)

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+const options = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  singleQuote: true,
+  semi: false,
+  tabWidth: 2,
+  trailingComma: 'none',
+  printWidth: 80
+}
+
+module.exports = options
